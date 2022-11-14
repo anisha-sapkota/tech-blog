85d474f1a4ef7a1c12803d5f20e4a549c1d73c62
Anisha Sapkota <anisha.sapkota79@gmail.com>
2022-11-14 21:23:45 +1100

    added models

---
+++ models/index.js
@@ -0,0 +1,30 @@
+// import models
+const Comment = require('./Comment');
+const Post = require('./Post');
+const User = require('./User');
+
+// Post belong to user
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+// User has many Post
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+
+// Comment belongs to a Post
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+// Post has many Comment
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
+module.exports = {
+  Post,
+  Comment,
+  User,
+};
